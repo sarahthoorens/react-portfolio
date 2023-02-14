a3743263f43290e3983ddba9bcecb44cd57f65e3
Sarah Thoorens <[s.thoorens@gmail.com]>
2023-02-13 21:16:30 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.40b90244fbd27c2dd152c93a6c2c39ab.js"
+  "/react-portfolio/precache-manifest.042f7660be01f1fedc6940e20d2304ce.js"
 );
 
 self.addEventListener('message', (event) => {
